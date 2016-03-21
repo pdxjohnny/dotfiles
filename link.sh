270e8132627027f159760a29d8296c60f41db1ad
John Andersen <john.s.andersen@intel.com>
2016-03-21 08:54:08 -0700

    Fixed color issues
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for file in $(ls -a | grep -v '.sh' | grep -vw '.git' | grep -vw '.' ); do
+for file in $(ls -a | grep -v '.sh' | grep -v '.md' | grep -vw '.git' | grep -vw '.' ); do
     ln -sv "$(pwd)/${file}" "${HOME}/${file}"
 done;
