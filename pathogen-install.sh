6d52b6aec4af1a909c42a2c1aed8044668d39b74
John Andersen <john.s.andersen@intel.com>
2016-03-21 08:03:50 -0700

    Dont link sh and update vim-go
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- pathogen-install.sh
+++ pathogen-install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+DIR=$(pwd)
+
 # pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # vim-go
 git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+cd ~/.vim/bundle/vim-go
+git pull
+cd $DIR
 
