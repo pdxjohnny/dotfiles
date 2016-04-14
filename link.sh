5804f049f99a145a3e4a568420de61a58937cb6c
John Andersen <johnandersenpdx@gmail.com>
2016-04-14 23:22:54 +0000

    Linking configs in folders
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- link.sh
+++ link.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Dotfiles
 for file in $(ls -a | grep -v '.sh' | grep -v '.md' | grep -vw '.git' | grep -vw '.' ); do
     ln -sv "$(pwd)/${file}" "${HOME}/${file}"
+done;
+
+# Configs
+CONFIG_DIR='configs'
+for dir in $(ls -a $CONFIG_DIR | grep -vw 'configs' | grep -vw '.' ); do
+    mkdir -pv "$HOME/$dir"
+    for file in $(ls -a "$CONFIG_DIR/$dir/" | grep -vw $dir | grep -vw '.' ); do
+        file_path="$PWD/$CONFIG_DIR/$dir/$file"
+        ln -sv "$file_path" "${HOME}/$dir/$file"
+    done;
 done;
